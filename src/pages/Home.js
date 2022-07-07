30c0f9096310429a2203e55d7471803de5ecc41d
lwto <linwathanoo227@gmail.com>
2022-07-07 18:33:32 +0630

    route home page and cuisine

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Vaggie from "../components/Vaggie";
+import Popular from "../components/Popular";
+
+function Home() {
+  return (
+    <div>
+      <Vaggie />
+      <Popular />
+    </div>
+  )
+}
+
+export default Home
