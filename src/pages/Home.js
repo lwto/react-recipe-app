36daf5f968009179aec380a95dbd56cb9c1b64a3
lwto <linwathanoo227@gmail.com>
2022-07-12 16:43:10 +0630

    add animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 import Vaggie from "../components/Vaggie";
 import Popular from "../components/Popular";
+import {motion} from 'framer-motion'
 
 function Home() {
   return (
-    <div>
+    <motion.div 
+      animate={{opacity: 1}}
+      initial={{opacity: 0}}
+      exit={{opacity: 0}}
+      transition={{duration:0.5}}
+    >
       <Vaggie />
       <Popular />
-    </div>
+    </motion.div>
   )
 }
 
